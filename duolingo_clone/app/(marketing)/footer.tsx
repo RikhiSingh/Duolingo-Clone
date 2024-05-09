a31c9593039f2e179f91aefcd19e02ac203df2e0
rikhisingh <rikhisinghnov@gmail.com>
2024-05-09 16:14:40 -0400

    added assets and completed the footer

--- duolingo_clone/app/(marketing)/footer.tsx
+++ duolingo_clone/app/(marketing)/footer.tsx
@@ -1,7 +1,71 @@
-export const Footer =() =>{
-    return(
-        <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            Footer
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export const Footer = () => {
+    return (
+        <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2 ">
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="hr.svg" 
+                        alt="Croatian" 
+                        height={32} 
+                        width={40} 
+                        className="mr-4 rounded -md"
+                        />
+                    Croatian
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="fr.svg" 
+                        alt="French" 
+                        height={32} 
+                        width={40} 
+                        className="mr-4 rounded -md"
+                        />
+                    French
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="jp.svg" 
+                        alt="Japan" 
+                        height={32} 
+                        width={40} 
+                        className="mr-4 rounded -md"
+                        />
+                    Japan
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="it.svg" 
+                        alt="Italy" 
+                        height={32} 
+                        width={40} 
+                        className="mr-4 rounded -md"
+                        />
+                    Italian
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="es.svg" 
+                        alt="Spain" 
+                        height={32} 
+                        width={40} 
+                        className="mr-4 rounded -md"
+                        />
+                    Spanish
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="in.svg" 
+                        alt="Spain" 
+                        height={32} 
+                        width={40} 
+                        className="mr-4 rounded -md"
+                        />
+                    Hindi
+                </Button>
+            </div>
         </footer>
     );
 };
--- duolingo_clone/app/(marketing)/page.tsx
+++ duolingo_clone/app/(marketing)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn, Practice and Master New languages with DuoLingo Clone.
         </h1>
-        <div>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
