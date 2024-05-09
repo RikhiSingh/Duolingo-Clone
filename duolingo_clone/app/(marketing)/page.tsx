a0657e9887459fd21282adee062fd2079fb62ab8
rikhisingh <rikhisinghnov@gmail.com>
2024-05-09 14:26:40 -0400

    header file created

--- duolingo_clone/app/(marketing)/page.tsx
+++ duolingo_clone/app/(marketing)/page.tsx
@@ -1,6 +1,9 @@
+import { Header } from "./header";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
+      <Header />
       <main className="flex-1 flex flex-col items-center justify-center">
         This is the marketing page
       </main>
