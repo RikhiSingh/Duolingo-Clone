a1a48123925d35e070fec417719021911d09675a
rikhisingh <rikhisinghnov@gmail.com>
2024-05-09 16:28:38 -0400

    fixed tailwind css and setup layout for main

--- duolingo_clone/app/(main)/layout.tsx
+++ duolingo_clone/app/(main)/layout.tsx
@@ -1,15 +1,19 @@
-type Props ={
+type Props = {
     children: React.ReactNode;
 };
 
 const MainLayout = ({
     children
 }: Props) => {
-    return ( 
-        <div>
-            {children}
-        </div>
-     );
+    return (
+        <>
+            <main className="pl-[256px] h-full">
+                <div className="h-full bg-red-500">
+                    {children}
+                </div>
+            </main>
+        </>
+    );
 }
- 
+
 export default MainLayout;
