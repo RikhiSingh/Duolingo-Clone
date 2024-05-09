2da5a893d7ed36e20f8c7cf26901219479a10429
rikhisingh <rikhisinghnov@gmail.com>
2024-05-09 16:44:41 -0400

    mobile sidebar created using sheet from clarkui

---
+++ duolingo_clone/components/mobile-sidebar.tsx
@@ -0,0 +1,21 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger
+} from '@/components/ui/sheet';
+
+import { Sidebar} from '@/components/sidebar';
+import { Menu } from 'lucide-react';
+
+export const MobileSidebar = () =>{
+    return(
+        <Sheet>
+            <SheetTrigger>
+                <Menu className='text-white'/>
+            </SheetTrigger>
+            <SheetContent>
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
+    )
+}
