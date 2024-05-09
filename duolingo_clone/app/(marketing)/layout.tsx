8965de0b5303ea7a68ec6a50f98625077ec3632e
rikhisingh <rikhisinghnov@gmail.com>
2024-05-09 14:29:53 -0400

    header and footer completed and fixed (skeleton)

--- duolingo_clone/app/(marketing)/layout.tsx
+++ duolingo_clone/app/(marketing)/layout.tsx
@@ -1,11 +1,18 @@
+import { Footer } from "./footer";
+import { Header } from "./header";
+
 type Props = {
     children: React.ReactNode;
 };
 
 const MarketingLayout = ({ children }: Props) => {
     return (
-        <div>
-            {children}
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1 flex flex-col items-center justify-center">
+                {children}
+            </main>
+            <Footer />
         </div>
     );
 }
--- duolingo_clone/app/(marketing)/page.tsx
+++ duolingo_clone/app/(marketing)/page.tsx
@@ -2,11 +2,8 @@ import { Header } from "./header";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1 flex flex-col items-center justify-center">
-        This is the marketing page
-      </main>
+    <div>
+      This is the marketing page
     </div>
   )
 }
