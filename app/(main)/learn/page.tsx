0d324cfaf95bd2b1430b76344384f7ff7f30b7e9
rikhisingh <rikhisinghnov@gmail.com>
2024-05-10 17:51:25 -0400

    loading and fetch user progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,13 +2,28 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
-    return ( 
+const LearnPage = async () => {
+
+    const userProgressData = getUserProgress();
+
+    const [
+        userProgress
+    ] = await Promise.all([
+        userProgressData
+    ]);
+
+    if(!userProgress || !userProgress.activeCourse){
+        redirect("/courses");
+    }
+
+    return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
-                <UserProgress 
-                    activeCourse={{ title:"Spanish", imageSrc: '/es.svg' }}
+                <UserProgress
+                    activeCourse={{ title: "Spanish", imageSrc: '/es.svg' }}
                     hearts={5}
                     points={100}
                     hasActiveSubscription={false}
@@ -18,7 +33,7 @@ const LearnPage = () => {
                 <Header title="Spanish" />
             </FeedWrapper>
         </div>
-     );
+    );
 }
- 
+
 export default LearnPage;
