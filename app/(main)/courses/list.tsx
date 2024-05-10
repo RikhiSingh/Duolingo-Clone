9f1c4b43d1e5f2f6666300e836a0a5ee77f152b6
rikhisingh <rikhisinghnov@gmail.com>
2024-05-10 17:47:09 -0400

    course selected and promise instead of await

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { courses } from "@/db/schema";
+import { courses, userProgress } from "@/db/schema";
 import { Card } from "./card";
 
 type Props = {
     courses: typeof courses.$inferSelect[];
-    activeCourseId: number;
+    activeCourseId?: typeof userProgress.$inferSelect.activeCourseId;
 };
 
 export const List = ({ courses, activeCourseId }: Props) => {
--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,10 +1,20 @@
-import { getCourses } from "@/db/queries";
+import { getCourses, getUserProgress } from "@/db/queries";
 
 import { List } from "./list";
 
 const CoursesPage = async () => {
 
-    const courses = await getCourses();
+    const coursesData = getCourses();
+    const userProgressData = getUserProgress();
+
+    // Promise helps with waterfall (best practice)
+    const [
+        courses,
+        userProgress,
+    ] = await Promise.all([
+        coursesData,
+        userProgressData,
+    ]);
 
     return (
         <div className="h-full max-w-[912px] px-3 mx-auto">
@@ -13,7 +23,7 @@ const CoursesPage = async () => {
             </h1>
             <List
                 courses={courses}
-                activeCourseId={1}
+                activeCourseId={userProgress?.activeCourseId}
             />
         </div>
     );
