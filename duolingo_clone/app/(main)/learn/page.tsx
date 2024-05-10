a24c4ab291c743cae0b0451ca00ee8d68455bc2f
rikhisingh <rikhisinghnov@gmail.com>
2024-05-09 23:22:01 -0400

    feed and sticky wrapper

--- duolingo_clone/app/(main)/learn/page.tsx
+++ duolingo_clone/app/(main)/learn/page.tsx
@@ -1,7 +1,15 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+
 const LearnPage = () => {
     return ( 
-        <div>
-            Learn
+        <div className="flex gap-[48px] px-6">
+            <FeedWrapper>
+                My Feed
+            </FeedWrapper>
+            <StickyWrapper>
+                My sticky sidebar
+            </StickyWrapper>
         </div>
      );
 }
