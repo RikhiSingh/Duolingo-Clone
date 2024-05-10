e24ba6949f04dbfb232e47c0e5eba8f65188b0d8
rikhisingh <rikhisinghnov@gmail.com>
2024-05-10 17:42:39 -0400

    query added for useProgress

--- db/queries.ts
+++ db/queries.ts
@@ -1,5 +1,25 @@
 import { cache } from "react";
 import db from "@/db/drizzle";
+import { auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { userProgress } from "@/db/schema";
+
+export const getUserProgress = cache(async()=>{
+    const {userId} = await auth();
+
+    if(!userId){
+        return null;
+    };
+
+    const data =await db.query.userProgress.findFirst({
+        where:eq(userProgress.userId, userId),
+        with:{
+            activeCourse:true,
+        },
+    });
+
+    return data;
+})
 
 export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany();
