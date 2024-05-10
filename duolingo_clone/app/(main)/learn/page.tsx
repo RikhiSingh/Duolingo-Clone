48b256e7e1b4ada84e8080a005d39c5a189ec96f
rikhisingh <rikhisinghnov@gmail.com>
2024-05-09 23:41:21 -0400

    header and user progress (learn page wrappers) finished

--- duolingo_clone/app/(main)/learn/page.tsx
+++ duolingo_clone/app/(main)/learn/page.tsx
@@ -1,12 +1,18 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
 
 const LearnPage = () => {
     return ( 
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
-                My sticky sidebar
+                <UserProgress 
+                    activeCourse={{ title:"Spanish", imageSrc: '/es.svg' }}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
             </StickyWrapper>
             <FeedWrapper>
                 <Header title="Spanish" />
