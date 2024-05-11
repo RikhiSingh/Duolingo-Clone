7ca6230d6070dcdc9668911555eb12af39dc9ec3
rikhisingh <rikhisinghnov@gmail.com>
2024-05-11 14:33:02 -0400

    lesson-button completed

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -101,9 +101,21 @@ export const LessonButton = ({
                         </CircularProgressbarWithChildren>
                     </div>
                 ) : (
-                    <div>
-                        Something
-                    </div>
+                    <Button
+                                size="rounded"
+                                variant={locked ? "locked" : "secondary"}
+                                className="h-[70px] w-[70px] border-b-8"
+                            >
+                                <Icon 
+                                    className={cn(
+                                        'h-10 w-10',
+                                        locked 
+                                        ? "fill-neutral-400 text-neutral-400 stroke-neutral-400"
+                                        : "fill-primary-foreground text-primary-foreground",
+                                        isCompleted && "fill-none stroke-[4]"
+                                    )}
+                                />
+                            </Button>
                 )}
             </div>
         </Link>
