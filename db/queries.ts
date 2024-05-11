ccb6872d726d467726e22ffc391d88c1eb6d1f0d
rikhisingh <rikhisinghnov@gmail.com>
2024-05-11 12:02:18 -0400

    queries fixed: get challenge for particular user not very user in database

--- db/queries.ts
+++ db/queries.ts
@@ -3,6 +3,7 @@ import db from "@/db/drizzle";
 import { auth } from "@clerk/nextjs";
 import { eq } from "drizzle-orm";
 import {
+    challengeProgress,
     courses,
     units,
     userProgress
@@ -26,12 +27,14 @@ export const getUserProgress = cache(async () => {
 });
 
 export const getUnits = cache(async () => {
+    const{userId} = await auth();
     const userProgress = await getUserProgress();
 
-    if (!userProgress?.activeCourseId) {
+    if (!userId || !userProgress?.activeCourseId) {
         return [];
     }
 
+    // TODO: Confirm whether order is needed or not
     const data = await db.query.units.findMany({
         where: eq(units.courseId, userProgress.activeCourseId),
         with: {
@@ -39,7 +42,12 @@ export const getUnits = cache(async () => {
                 with: {
                     challenges: {
                         with: {
-                            challengeProgress: true,
+                            challengeProgress: {
+                                where: eq(
+                                    challengeProgress.userId,
+                                    userId,
+                                ),
+                            },
                         },
                     },
                 },
