f807547b3a9804563b20f8c715ce1d4a803ec287
rikhisingh <rikhisinghnov@gmail.com>
2024-05-11 00:38:55 -0400

    header and logo fixed for individual course page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -15,6 +15,7 @@ const LearnPage = async () => {
         userProgressData
     ]);
 
+    // if no check we will need ? for typeof as well as where there is userProgress.active etc.
     if(!userProgress || !userProgress.activeCourse){
         redirect("/courses");
     }
@@ -23,14 +24,14 @@ const LearnPage = async () => {
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
                 <UserProgress
-                    activeCourse={{ title: "Spanish", imageSrc: '/es.svg' }}
-                    hearts={5}
-                    points={100}
+                    activeCourse={userProgress.activeCourse}
+                    hearts={userProgress.hearts}
+                    points={userProgress.points}
                     hasActiveSubscription={false}
                 />
             </StickyWrapper>
             <FeedWrapper>
-                <Header title="Spanish" />
+                <Header title={userProgress.activeCourse.title} />
             </FeedWrapper>
         </div>
     );
