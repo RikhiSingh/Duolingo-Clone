fd8719d4a2699ae53c97169ee25e43d01fe82359
rikhisingh <rikhisinghnov@gmail.com>
2024-05-11 14:31:41 -0400

    seeded temp lessons

--- app/(main)/learn/unit.tsx
+++ app/(main)/learn/unit.tsx
@@ -30,7 +30,7 @@ export const Unit = ({
             <UnitBanner title={title} description={description}/>
             <div className="flex items-center flex-col relative">
                 {lessons.map((lesson,index)=>{
-                    const isCurrent = true || lesson.id === activeLesson?.id; // TODO: remove later
+                    const isCurrent = lesson.id === activeLesson?.id; 
                     const isLocked = !lesson.completed && !isCurrent;
 
                     return (
