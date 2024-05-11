446ce5cbab97e9d8adccffc79be4a1f66725a9b3
rikhisingh <rikhisinghnov@gmail.com>
2024-05-11 16:13:07 -0400

    empty lessons set as uncompleted

--- db/queries.ts
+++ db/queries.ts
@@ -58,6 +58,11 @@ export const getUnits = cache(async () => {
 
     const normalizedData = data.map((unit) => {
         const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+            // We wont publish any empty lessons but if they is its completed will be as false
+            if (lesson.challenges.length === 0) {
+                return { ...lesson, completed: false };
+            }
+
             const allCompletedChallenges = lesson.challenges.every((challenge) => {
                 return challenge.challengeProgress
                     && challenge.challengeProgress.length > 0
