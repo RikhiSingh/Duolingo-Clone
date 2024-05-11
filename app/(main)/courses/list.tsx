058ae7c6a098b27fd177e2efd261fa870a4ae01e
rikhisingh <rikhisinghnov@gmail.com>
2024-05-10 22:38:45 -0400

    added toast for error

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -5,6 +5,7 @@ import { Card } from "./card";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import { upsertUserProgress } from "@/actions/user-progress";
+import { toast } from "sonner";
 
 type Props = {
     courses: typeof courses.$inferSelect[];
@@ -23,7 +24,8 @@ export const List = ({ courses, activeCourseId }: Props) => {
         }
 
         startTransition(()=>{
-            upsertUserProgress(id);
+            upsertUserProgress(id)
+                .catch(()=> toast.error("Something went wrong!"))
         });
     }
 
