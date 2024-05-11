1d5b3ab32c8f88672488d230857415d73a749be3
rikhisingh <rikhisinghnov@gmail.com>
2024-05-11 14:14:20 -0400

    added react progress bar and calc indentation logic

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -1,18 +1,52 @@
 "use client";
 
-type Props={
-    id:number;
-    index:number;
+import { Check, Crown, Star } from "lucide-react";
+
+type Props = {
+    id: number;
+    index: number;
     totalCount: number;
-    locked?:boolean;
-    current?:boolean;
+    locked?: boolean;
+    current?: boolean;
     percentage: number;
 };
 
-export const LessonButton =({}:Props)=>{
-    return(
-        <div>
+export const LessonButton = ({
+    id,
+    index,
+    totalCount,
+    locked,
+    current,
+    percentage,
+}: Props) => {
+    const cycleLength = 8;
+    const cycleIndex = index % cycleLength;
 
+    let indentationLevel;
+
+    if (cycleIndex <= 2) {
+        indentationLevel = cycleIndex;
+    } else if (cycleIndex <= 4) {
+        indentationLevel = 4 - cycleIndex;
+    } else if (cycleIndex <= 6) {
+        indentationLevel = 4 - cycleIndex;
+    } else {
+        indentationLevel = cycleIndex - 8;
+    }
+
+    const rightPosition = indentationLevel * 40;
+
+    const isFirst = index ===0;
+    const isLast = index === totalCount;
+    const isCompleted = !current && !locked;
+
+    const Icon = isCompleted ? Check : isLast ? Crown : Star;
+
+    const href = isCompleted ? `/lesson/${id}` : "/lesson";
+
+    return (
+        <div>
+            {id}
         </div>
     );
 };
