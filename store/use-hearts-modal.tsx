ed539912f5e2fe527fb647ae0175c97624fe3332
rikhisingh <rikhisinghnov@gmail.com>
2024-05-14 22:40:03 -0400

    hearts modal

--- store/use-hearts-modal.tsx
+++ store/use-hearts-modal.tsx
@@ -1,12 +1,12 @@
 import { create } from "zustand";
 
-type HeartModalState = {
+type HeartsModalState = {
     isOpen: boolean;
     open: () => void;
     close: () => void;
 };
 
-export const useHeartModal = create<HeartModalState>((set) => ({
+export const useHeartsModal = create<HeartsModalState>((set) => ({
     isOpen: false,
     open: () => set({ isOpen: true }),
     close: () => set({ isOpen: false }),
