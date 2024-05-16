db6b881a611c2f3cbfcbec02c845da6255635118
rikhisingh <rikhisinghnov@gmail.com>
2024-05-16 13:11:19 -0400

    added userprogress

--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -20,7 +20,10 @@ const ShopPage = async () => {
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
                 <UserProgress
-
+                    activeCourse={userProgress.activeCourse}
+                    hearts={userProgress.hearts}
+                    points={userProgress.points}
+                    hasActiveSubscription={false}
                 />
             </StickyWrapper>
         </div>
