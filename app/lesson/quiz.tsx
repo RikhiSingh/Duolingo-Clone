09a9225c18ec0c835cf28d7b59d42e32c74e00ee
rikhisingh <rikhisinghnov@gmail.com>
2024-05-16 12:10:44 -0400

    added favicon and fixed finish logo

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -171,14 +171,14 @@ export const Quiz = ({
                 />
                 <div className="flex flex-col gap-y-4 lg:gap-y-8 max-w-lg mx-auto text-center items-center justify-center h-full">
                     <Image
-                        src="finish.svg"
+                        src="/finish.svg"
                         alt="finish"
                         className="hidden lg:block"
                         height={100}
                         width={100}
                     />
                     <Image
-                        src="finish.svg"
+                        src="/finish.svg"
                         alt="finish"
                         className="block lg:hidden"
                         height={50}
