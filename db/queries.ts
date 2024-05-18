e289b874db2b984397893ac621322b5dfeb13b94
rikhisingh <rikhisinghnov@gmail.com>
2024-05-18 10:03:19 -0400

    ordering units, lessons and challenges in ascendign order

--- db/queries.ts
+++ db/queries.ts
@@ -37,12 +37,15 @@ export const getUnits = cache(async () => {
     }
 
     // TODO: Confirm whether order is needed or not
-    const data = await db.query.units.findMany({
+    const data = await db.query.units.findMany({        
+        orderBy: (units, { asc }) => [asc(units.order)],
         where: eq(units.courseId, userProgress.activeCourseId),
         with: {
             lessons: {
+                orderBy: (lessons, { asc }) => [asc(lessons.order)],
                 with: {
                     challenges: {
+                        orderBy: (challenges, { asc }) => [asc(challenges.order)],
                         with: {
                             challengeProgress: {
                                 where: eq(
@@ -87,12 +90,12 @@ export const getCourses = cache(async () => {
 export const getCourseById = cache(async (courseId: number) => {
     const data = await db.query.courses.findFirst({
         where: eq(courses.id, courseId),
-        with:{
-            units:{
-                orderBy: (units,{asc}) => [asc(units.order)],
-                with:{
+        with: {
+            units: {
+                orderBy: (units, { asc }) => [asc(units.order)],
+                with: {
                     lessons: {
-                        orderBy: (lessons, {asc}) => [asc(lessons.order)],
+                        orderBy: (lessons, { asc }) => [asc(lessons.order)],
                     },
                 },
             },
@@ -225,12 +228,12 @@ export const getUserSubscription = cache(async () => {
 
     if (!data) return null;
 
-    const isActive = 
+    const isActive =
         data.stripePriceId &&
         // month and an extra day
         data.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
 
-    return{
+    return {
         ...data,
         isActive: !!isActive,
     };
